0796c6b1aa061b265176a39aaa8e9ca493d049fe
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2024-09-25 09:57:42 +0100

    filter history javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,79 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
+
+function show_filing_type() {
+  var filing_type_checkbox = document.getElementById('show-filing');
+  var filing_type_cols = document.getElementsByName('type_cols');
+
+  if (filing_type_checkbox.checked) {
+    for (let i = 0; i < filing_type_cols.length; i++)
+      filing_type_cols[i].classList.remove('govuk-visually-hidden');
+  } else {
+    for (let i = 0; i < filing_type_cols.length; i++)
+      filing_type_cols[i].classList.add('govuk-visually-hidden');
+  }
+}
+
+
+function filter_table() {
+  var row_visible = false;
+  var some_rows_visible = false;
+  var checked_boxes_list = [];
+  var checkboxes = document.getElementsByClassName("govuk-checkboxes__input");
+  var history_table = document.getElementById("history_table");
+  var no_results_paragraph = document.getElementById("no_results_paragraph");
+
+  //loop around small checkboxes and if checked add name to list
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].name != 'show-filing') {
+      if (checkboxes[i].checked) {
+        checked_boxes_list.push(checkboxes[i].name);
+      }
+    }
+  }
+
+  //no checked boxes so all rows visible
+  if (checked_boxes_list.length == 0 ) {
+    row_visible = true;
+    some_rows_visible = true;
+  }
+
+  //get all table rows
+  var table_rows = document.getElementsByClassName("govuk-table__row");
+
+  //loop through all rows
+  for (let i = 0; i < table_rows.length; i++) {
+
+    //loop around checked checkboxes list and see if row name matches any checked 
+    for (let j = 0; j < checked_boxes_list.length; j++) {
+      if (table_rows[i].getAttribute('name') == checked_boxes_list[j]) {
+        row_visible = true;
+        some_rows_visible = true;
+        break;
+      } else {
+        row_visible = false;
+      }
+    }
+
+    if (row_visible ) {
+      table_rows[i].classList.remove('govuk-visually-hidden')
+    } else {
+      table_rows[i].classList.add('govuk-visually-hidden');
+    }
+
+    if (table_rows[i].getAttribute('name') == 'header'){
+      table_rows[i].classList.remove('govuk-visually-hidden');
+    }
+
+  }
+
+  //show 'no results' paragraph and hide table if no matching rows
+  if (some_rows_visible) {
+    history_table.classList.remove('govuk-visually-hidden')
+    no_results_paragraph.classList.add('govuk-visually-hidden')
+  } else {
+    history_table.classList.add('govuk-visually-hidden');
+    no_results_paragraph.classList.remove('govuk-visually-hidden')
+  }
+}
--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -15,370 +15,63 @@ router.use((req, res, next) => {
   next()  
 }) 
 
-// ******* Sign in email validation ********************************
-router.get('/v1/sign-in-email', function (req, res) {
-    // Set URl
-    res.render('v1/sign-in-email', {
-      currentUrl: req.originalUrl
-    })
+// ******* start javascript *********************
+router.get('/v1/start', function (req, res) {
+  // Set URl
+  res.render('v1/start', {
+    currentUrl: req.originalUrl
   })
-  
-router.post('/v1/sign-in-email', function (req, res) {
+})
+
+router.post('/v1/start', function (req, res) {
+  res.redirect('/v1/search-results-all')
+})
+
+
+// ******* sign-in validation ********************************
+router.get('/v1/sign-in', function (req, res) {
+  // Set URl
+  res.render('v1/sign-in', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v1/sign-in', function (req, res) {
   // Create empty array and set error variables to false
   var errors = []
+  var emailHasError = false
+  var passwordHasError = false
 
-  if (req.session.data['signin-email'] === '') {
+  // Check if user has filled out a email
+  if (req.session.data['emailAddress'] === '') {
     // No value so add error to array
+    emailHasError = true
     errors.push({
       text: 'Enter your email address',
-      href: '#signin-email'
+      href: '#emailAddress'
     })
   }
 
-  if (req.session.data['signin-email'] === '') {
-    // Re-show page with error value as true so errors will show
-    res.render('v1/sign-in-email', {
-      errorSigninEmail: true,
-      errorList: errors
-    })
-  } else {
-    // User inputted value so move to next page
-    res.redirect('/v1/sign-in-password')
-  }
-})
-  
-  
-// ******* Sign in password validation ********************************
-router.get('/v1/sign-in-password', function (req, res) {
-  // Set URl
-  res.render('v1/sign-in-password', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/sign-in-password', function (req, res) {
-  // Create empty array and set error variables to false
-  var errors = []
-
-  if (req.session.data['signin-password'] === '') {
+  // Check if user has filled out a password
+  if (req.session.data['password'] === '') {
     // No value so add error to array
+    passwordHasError = true
     errors.push({
       text: 'Enter your password',
-      href: '#signin-password'
+      href: '#password'
     })
   }
 
-  if (req.session.data['signin-password'] === '') {
+  // Check if eother filed not filled out
+  if (emailHasError || passwordHasError) {
     // Re-show page with error value as true so errors will show
-    res.render('v1/sign-in-password', {
-      errorSigninPassword: true,
+    res.render('v1/sign-in', {
+      errorEmail: emailHasError,
+      errorPassword: passwordHasError,
       errorList: errors
     })
   } else {
     // User inputted value so move to next page
-    res.redirect('/v1/confirm-for')
+    res.redirect('/v1/start')
   }
-})
-
-
-// ******* confirm-for javascript ********************************
-router.get('/v1/confirm-for', function (req, res) {
-  // Set URl
-  res.render('v1/confirm-for', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/confirm-for', function (req, res) {
-  // Create empty array
-  var errors = []
-
-  if (typeof req.session.data['confirmFor'] === 'undefined') {
-    // No value so add error to array
-    errors.push({
-      text: 'Select if you need to confirm yourself as a PSC',
-      href: '#confirmFor'
-    })
-
-    // Re-show page with error value as true so errors will show
-    res.render('v1/confirm-for', {
-      errorConfirmFor: true,
-      errorList: errors
-    })
-  } else {
-   
-    if ((req.session.data['confirmFor'] == 'yes')) {
-      if (req.session.data['signin-email'] == 'email@server.com') {
-        //  happy path
-        res.redirect('/v1/confirm-psc')
-      } else {
-        res.redirect('/v1/company-number')
-      }
-    } else {
-      //  Confirming someone else
-      res.redirect('/v1/personal-code')
-    }
-  }
-})
-
-
-// ******* name javascript ********************************
-router.get('/v1/company-number', function (req, res) {
-  // Set URl
-  res.render('v1/company-number', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/company-number', function (req, res) {
-  // Create empty array and set error variables to false
-  var errors = [];
-
-  if (req.session.data['companyNumber'] === '') {
-    // No value so add error to array
-    errors.push({
-      text: 'Enter the company number',
-      href: '#companyNumber'
-    })
-
-    // Re-show page with error value as true so errors will show
-    res.render('v1/company-number', {
-      errorCompanyNumber: true,
-      errorList: errors
-    })
-  } else {
-      res.redirect('/v1/confirm-company')
-  }
-})
-
-// ******* confirm-company javascript ********************************
-router.get('/v1/confirm-company', function (req, res) {
-  // Set URl
-  res.render('v1/confirm-company', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/confirm-company', function (req, res) {
-  if ((req.session.data['companyNumber'] == '12345678') 
-  || (req.session.data['companyNumber'] == '11112222')
-  || (req.session.data['companyNumber'] == '23232323')) {
-    res.redirect('/v1/confirm-psc')
-    
-  } else if ((req.session.data['companyNumber'] == '44445555')) {
-    res.redirect('/v1/how-name-recorded')
-
-  } else {
-    res.redirect('/v1/psc-cannot-find-details')
-  }
-})
-
-
-// ******* confirm-psc javascript ********************************
-router.get('/v1/confirm-psc', function (req, res) {
-  // Set URl
-  res.render('v1/confirm-psc', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/confirm-psc', function (req, res) {
-  // Create empty array
-  var errors = []
-
-  if (typeof req.session.data['confirmPsc'] === 'undefined') {
-    // No value so add error to array
-    errors.push({
-      text: 'Select if this is the correct PSC',
-      href: '#confirmPsc'
-    })
-    
-
-    // Re-show page with error value as true so errors will show
-    res.render('v1/confirm-psc', {
-      errorConfirmPsc: true,
-      errorList: errors
-    })
-  } else {
-    if (req.session.data['confirmPsc'] == 'yes') {
-      //  happy path
-      if ((req.session.data['companyNumber'] == '12345678')
-      || (req.session.data['companyNumber'] == '10101010')) {
-        res.redirect('/v1/psc-statement')
-        
-      //  confirmation statement not due yet
-      } else if ((req.session.data['companyNumber'] == '11112222')) {
-        res.redirect('/v1/psc-cannot-confirm-yet')
-
-      //  name mismatch
-      } else if ((req.session.data['companyNumber'] == '23232323') 
-              || (req.session.data['companyNumber'] == '44445555')) {
-        res.redirect('/v1/why-this-name')
-
-      //  multiple dob matches
-      } else {
-        res.redirect('/v1/psc-did-not-confirm')
-      }
-    } else {
-      res.redirect('/v1/psc-did-not-confirm')
-    }
-  }
-})
-
-
-// ******* psc-statement javascript ********************************
-router.get('/v1/psc-statement', function (req, res) {
-  // Set URl
-  res.render('v1/psc-statement', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/psc-statement', function (req, res) {
-  // Create empty array
-  var errors = []
-
-  if (typeof req.session.data['verificationStatement'] === 'undefined') {
-    // No value so add error to array
-    errors.push({
-      text: 'Confirm if the verification statement is correct',
-      href: '#verificationStatement'
-    })
-
-    // Re-show page with error value as true so errors will show
-    res.render('v1/psc-statement', {
-      errorStatement: true,
-      errorList: errors
-    })
-  } else {
-    res.redirect('/v1/psc-confirmed')
-  }
-})
-
-
-// ******* why-this-name javascript ********************************
-router.get('/v1/why-this-name', function (req, res) {
-  // Set URl
-  res.render('v1/why-this-name', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/why-this-name', function (req, res) {
-  // Create empty array
-  var errors = []
-
-  if (typeof req.session.data['whyThisName'] === 'undefined') {
-    // No value so add error to array
-    errors.push({
-      text: 'Select why you use this name',
-      href: '#whyThisName'
-    })
-    
-
-    // Re-show page with error value as true so errors will show
-    res.render('v1/why-this-name', {
-      errorwhyThisName: true,
-      errorList: errors
-    })
-  } else {
-      res.redirect('/v1/psc-statement')
-  }
-})
-
-
-// ******* how-name-recorded validation ********************************
-router.get('/v1/how-name-recorded', function (req, res) {
-  // Set URl
-  res.render('v1/how-name-recorded', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/how-name-recorded', function (req, res) {
-  var errors = []
-  var firstNameError = false
-  var lastNameError = false
-
-  var nameError = false
-
-  // Check if user has filled out first name
-  if (req.session.data['firstName'] === '') {
-    // No value so add error to array
-    firstNameError = true
-    nameError = true
-    errors.push({
-      text: 'Enter the first name',
-      href: '#firstName'
-    })
-  }
-
-  // Check if user has filled out last name
-  if (req.session.data['lastName'] === '') {
-    // No value so add error to array
-    lastNameError = true
-    nameError = true
-    errors.push({
-      text: 'Enter the last name',
-      href: '#lastName'
-    })
-  }
-
-  if (nameError) {
-    // Re-show page with error value as true so errors will show
-    res.render('v1/how-name-recorded', {
-      errorFirstName: firstNameError,
-      errorLastName: lastNameError,
-      errorNameRecorded: nameError,
-      errorList: errors
-    })
-  } else {
-    res.redirect('/v1/confirm-psc')
-  }
-
-})
-
-
-// ******* Sign in email validation ********************************
-router.get('/v1/personal-code', function (req, res) {
-  // Set URl
-  res.render('v1/personal-code', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/personal-code', function (req, res) {
-// Create empty array and set error variables to false
-var errors = []
-
-if (req.session.data['personalCode'] === '') {
-  // No value so add error to array
-  errors.push({
-    text: 'Enter the Companies House personal code',
-    href: '#personalCode'
-  })
-}
-
-if (req.session.data['personalCode'] === '') {
-  // Re-show page with error value as true so errors will show
-  res.render('v1/personal-code', {
-    errorCode: true,
-    errorList: errors
-  })
-} else {
-  res.redirect('/v1/company-number')
-}
-})
-
-
-// ******* how-name-recorded validation ********************************
-router.get('/v1/psc-confirmed', function (req, res) {
-  // Set URl
-  res.render('v1/psc-confirmed', {
-    currentUrl: req.originalUrl
-  })
-})
-
-router.post('/v1/psc-confirmed', function (req, res) {
-    res.redirect('/v1/personal-code')
 })
