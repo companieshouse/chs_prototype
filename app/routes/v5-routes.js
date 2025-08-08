116e9f3db53dcc206944bf248dbd115cd13165a5
phoeberadford <pradford@companieshouse.gov.uk>
2025-08-08 09:39:02 +0100

    v5 creation

---
+++ app/routes/v5-routes.js
@@ -0,0 +1,77 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+
+module.exports=router;
+
+// Show session data and URLs in the terminal  
+router.use((req, res, next) => {  
+  const log = {  
+    method: req.method,  
+    url: req.originalUrl,  
+    data: req.session.data  
+  }  
+  console.log(JSON.stringify(log, null, 2))  
+  next()  
+}) 
+
+// ******* start javascript *********************
+router.get('/v5/start', function (req, res) {
+  // Set URl
+  res.render('v5/start', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v5/start', function (req, res) {
+  res.redirect('/v5/search-results')
+})
+
+
+// ******* sign-in validation ********************************
+router.get('/v5/sign-in', function (req, res) {
+  // Set URl
+  res.render('v5/sign-in', {
+    currentUrl: req.originalUrl
+  })
+})
+
+router.post('/v5/sign-in', function (req, res) {
+  // Create empty array and set error variables to false
+  var errors = []
+  var emailHasError = false
+  var passwordHasError = false
+
+  // Check if user has filled out a email
+  if (req.session.data['emailAddress'] === '') {
+    // No value so add error to array
+    emailHasError = true
+    errors.push({
+      text: 'Enter your email address',
+      href: '#emailAddress'
+    })
+  }
+
+  // Check if user has filled out a password
+  if (req.session.data['password'] === '') {
+    // No value so add error to array
+    passwordHasError = true
+    errors.push({
+      text: 'Enter your password',
+      href: '#password'
+    })
+  }
+
+  // Check if eother filed not filled out
+  if (emailHasError || passwordHasError) {
+    // Re-show page with error value as true so errors will show
+    res.render('v5/sign-in', {
+      errorEmail: emailHasError,
+      errorPassword: passwordHasError,
+      errorList: errors
+    })
+  } else {
+    // User inputted value so move to next page
+    res.redirect('/v5/start')
+  }
+})
